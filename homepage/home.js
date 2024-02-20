ba0bb1b080ebdf26889ae4128ae41349ba8e2cfd
DoGGoverHoEs <oxgodxo2002@gmail.com>
2024-02-20 23:38:12 +0700

    Initial commit

---
+++ homepage/home.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", e => {
+    const isDropDownButton = e.target.matches("[data-dropdown-button]")     
+    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
+    let currentDropDown
+    if (isDropDownButton) {
+        currentDropDown = e.target.closest('[data-dropdown]')
+        currentDropDown.classList.toggle('active')
+    }
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropDown) return
+        dropdown.classList.remove('active');
+    })
+})
